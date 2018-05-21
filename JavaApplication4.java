164a0c12ffb966fb803663054ef07ad2c9569d71
ldss2002 <32168991+ldss2002@users.noreply.github.com>
2018-05-21 11:51:55 +0100

    Add files via upload

---
+++ JavaApplication4.java
@@ -0,0 +1,43 @@
+package javaapplication4;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import java.util.List;
+import java.util.Scanner;
+
+public class JavaApplication4 {
+private static List <String> assinaturas;
+	
+	public static void main(String[] args) {
+		assinaturas = new ArrayList <String>();
+		Scanner dados = new Scanner (System.in);
+		int menu;
+		String inserir;
+		do {
+		System.out.print("..:Assinaturas:.."
+				+"\n(1) Adicionar Assinaturas \n"
+				+"(2) Visualizar Assisnaturas \n"
+				+"(0) Sair do programa \n"
+				+"\nInsira a opção que deseja: ");
+		menu = dados.nextInt();
+		switch(menu) {
+			case 1: 
+					System.out.println();
+					inserir=JOptionPane.showInputDialog("Assinatura: ");
+					assinaturas.add(inserir);
+					System.out.println();
+					System.out.println(assinaturas);
+					System.out.println();
+			break;
+			case 2:
+					System.out.println();
+					System.out.println(assinaturas);
+					System.out.println();
+			break;
+			case 0:
+					System.exit(0);
+					
+		}
+		} while (true);
+
+	}
+	}
