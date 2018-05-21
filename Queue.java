8995b861850e89867e38713a8ce2fd6490be0b88
ldss2002 <32168991+ldss2002@users.noreply.github.com>
2018-05-21 11:54:03 +0100

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,26 +1,40 @@
 package queue;
 import java.util.PriorityQueue;
-public class Queue {
+import java.util.Scanner;
+public class queue {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
+		Scanner LerS= new Scanner(System.in);
+		PriorityQueue <Integer> pq1 = new PriorityQueue <Integer>();
+	int variavel;
+	System.out.print("Qual é a primeira variavel que quer introduzir?");
+	variavel = LerS.nextInt();
+	pq1.add(variavel);
+	int variavel1;
+	System.out.print("Qual é a segunda variavel que quer introduzir?");
+	variavel1 = LerS.nextInt();
+	pq1.add(variavel1);
+	int variavel2;
+	System.out.print("Qual é a terceira variavel que introduzir?");
+	variavel2 = LerS.nextInt();
+	pq1.add(variavel2);
+	int variavel3;
+	System.out.print("Qual é a quarta variavel que quer introduzir?");
+	variavel3 = LerS.nextInt();
+	pq1.add(variavel3);
+	int variavel4;
+	System.out.print("Qual é a quinta variavel que quer introduzir?");
+	variavel4 = LerS.nextInt();
+	pq1.add(variavel4);
+	int variavel5;
+	System.out.print("Qual é a sexta variavel que quer introduzir?");
+	variavel5 = LerS.nextInt();
+	pq1.add(variavel5);
+	System.out.print ("Fila Original:" +pq1);
+	System.out.print("Avariavel à cabeça:" + pq1.element());
+	
+	
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        // Add data.
-        queue.add(5);
-        queue.add(2);
-        queue.add(8);
-        queue.add(1);
-        queue.add(2);
-        queue.add(11);
-       
-        System.out.println("O elemento à cabeça:" + queue.element());
-        while (queue.size() > 0) {
-        System.out.println(queue.remove());
-            Integer removedElement = queue.poll();
-		System.out.println("removedElement : " + removedElement);
-		System.out.println("queue : " + queue + "\n");
-        
-                
-  }  }
+	}
+
 }
-
