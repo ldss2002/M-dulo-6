e0a3563ee32d599b08e71a6d55595833bb462a34
ldss2002 <32168991+ldss2002@users.noreply.github.com>
2018-05-21 11:46:14 +0100

    Add files via upload

---
+++ Queue.java
@@ -0,0 +1,26 @@
+package queue;
+import java.util.PriorityQueue;
+public class Queue {
+
+  public static void main(String[] args) {
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        // Add data.
+        queue.add(5);
+        queue.add(2);
+        queue.add(8);
+        queue.add(1);
+        queue.add(2);
+        queue.add(11);
+       
+        System.out.println("O elemento à cabeça:" + queue.element());
+        while (queue.size() > 0) {
+        System.out.println(queue.remove());
+            Integer removedElement = queue.poll();
+		System.out.println("removedElement : " + removedElement);
+		System.out.println("queue : " + queue + "\n");
+        
+                
+  }  }
+}
+
